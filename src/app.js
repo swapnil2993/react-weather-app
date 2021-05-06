b7aee88e04ef8c036f7df8624aab3fe39f445ccc
swapnil2993 <swapnilpande96@gmail.com>
2021-05-07 01:56:34 +0530

    disabling csp temp

--- src/app.js
+++ src/app.js
@@ -1,27 +1,27 @@
-const createError =require('http-errors');
-const cors =require('cors');
-const express =require('express');
-const helmet =require('helmet');
-const path =require('path');
-const Middlewares =require('./middleware');
+const createError = require('http-errors')
+const cors = require('cors')
+const express = require('express')
+const helmet = require('helmet')
+const path = require('path')
+const Middlewares = require('./middleware')
 
-let app = express();
+let app = express()
 
-app.use(helmet());
-app.disable('x-powered-by');
-app.set('view-engine', 'jade');
+app.use(helmet({ contentSecurityPolicy: false }))
+app.disable('x-powered-by')
+app.set('view-engine', 'jade')
 
-app.use(express.static(path.join(__dirname, '../webclient/build')));
-app.use(cors());
-app.options('*', cors());
-app.use(Middlewares.configuration);
+app.use(express.static(path.join(__dirname, '../webclient/build')))
+app.use(cors())
+app.options('*', cors())
+app.use(Middlewares.configuration)
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '../webclient/build/index.html'));
-});
+  res.sendFile(path.join(__dirname + '../webclient/build/index.html'))
+})
 
 module.exports = app
